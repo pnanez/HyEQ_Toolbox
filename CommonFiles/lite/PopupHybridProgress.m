7728118892aeccfd997cd6e1a8fb92c56142ced6
Paul Wintz <pwintz@ucsc.edun>
2021-07-16 16:21:04 -0700

    Optimization: Change Progressbar to only show updates if at least 0.25 seconds have passed.

--- CommonFiles/lite/PopupHybridProgress.m
+++ CommonFiles/lite/PopupHybridProgress.m
@@ -2,21 +2,25 @@ classdef PopupHybridProgress < HybridProgress
     
     properties
         progressbar; 
+        update_tic;
     end
     
     methods 
 
         function init(this, tspan, jspan)
             this.progressbar = waitbar(0.0, "");
+            this.update_tic = tic();
             init@HybridProgress(this, tspan, jspan);
         end
 
         function update(this)   
             assert(~isempty(this.progressbar), "Progress has not been initialized. Call init() before update()")
             pct = max(this.t_percent, this.j_percent);
-            msg = sprintf("Solving Hybrid Sysytem. t=%.3f, j=%d", this.t, this.j);
-            if isvalid(this.progressbar) && this.progressbar.Visible
+            show_update = toc(this.update_tic) > 0.25;
+            if isvalid(this.progressbar) && this.progressbar.Visible && show_update
+                msg = sprintf("Solving Hybrid Sysytem. t=%.3f, j=%d", this.t, this.j);
                 waitbar(pct, this.progressbar, msg)
+                this.update_tic = tic();
             end
         end
 
