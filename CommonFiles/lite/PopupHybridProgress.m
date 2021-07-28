073638b1a0c3293383ad030943137cad2c0cef98
Paul Wintz <pwintz@ucsc.edun>
2021-07-28 10:46:29 -0700

    Delay showing PopupHybridProgress until min_delay time has passed

--- CommonFiles/lite/PopupHybridProgress.m
+++ CommonFiles/lite/PopupHybridProgress.m
@@ -15,9 +15,13 @@ classdef PopupHybridProgress < HybridProgress
     methods 
 
         function init(this, tspan, jspan)
-            this.progressbar = waitbar(0.0, "", ...
-                                "Name", "Hybrid Solver Progress", ...
-                                "CreateCancelBtn", @(src, event) this.cancelSolver());
+            % Creating the waitbar is slow, so we wait to initialize it 
+            % until a time of at least min_delay seconds has passed. This
+            % reduces the overhead for computing short simulations that
+            % complete withing min_delay seconds because the waitbar will
+            % then never open. This improves the startup time by a factor
+            % of ~5x. 
+            this.progressbar = []; % Must be explicitly set to empty so we know to create the waitbar.
             this.last_update_tic = tic();
             this.last_update_t = -Inf;
             this.last_update_j = -Inf;
@@ -25,16 +29,20 @@ classdef PopupHybridProgress < HybridProgress
         end
 
         function update(this)   
-            assert(~isempty(this.progressbar), ...  
-                    "Progress has not been initialized. Call init() before update()")
             pct = max(this.t_percent, this.j_percent);
             round_t = floor(10^this.t_decimal_places * this.t) / 10^this.t_decimal_places;
+            
             % Updating the progress bar is slow, so we wait until
             % 'min_delay' seconds have passed and either the value of j or 
             % the displayed (rounded) value of 't' has changed. 
+            % The speedup from this optimization depends on the
+            % system being solved and the choices of min_delay and
+            % t_decimal places, but we've seen improvements on the order of 5x. 
             show_update = toc(this.last_update_tic) > this.min_delay ...
                             && (round_t > this.last_update_t || this.j > this.last_update_j);
-            if isvalid(this.progressbar) && this.progressbar.Visible && show_update
+            
+            if show_update
+                this.openWaitbar()
                 msg = sprintf("Solving Hybrid Sysytem. t=%." + this.t_decimal_places + "f, j=%d", this.t, this.j);
                 waitbar(pct, this.progressbar, msg)
                 this.last_update_t = round_t;
@@ -44,12 +52,20 @@ classdef PopupHybridProgress < HybridProgress
         end
 
         function done(this)    
-            if isvalid(this.progressbar) && this.progressbar.Visible
+            if ~isempty(this.progressbar) && isvalid(this.progressbar) && this.progressbar.Visible
                 close(this.progressbar)
                 delete(this.progressbar)
             end
         end
        
+        function openWaitbar(this)
+            if isempty(this.progressbar)
+                this.progressbar = waitbar(0.0, "", ...
+                    "Name", "Hybrid Solver Progress", ...
+                    "CreateCancelBtn", @(src, event) this.cancelSolver());
+            end
+        end
+        
         function cancelSolver(this)
             this.cancel = true;
         end
