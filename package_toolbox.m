893df1d7871cdd7c530334c70789d951c7d99e74
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2021-07-29 23:47:25 -0700

    Update .gitignore and GettingStarted.mlx, remote .DS_Store files

--- package_toolbox.m
+++ package_toolbox.m
@@ -1,2 +1,13 @@
-projectFile = 'Hybrid Equations Toolbox.prj';
+close all
+
+% Set up the path
+addpath('CommonFiles/lite', ...
+    'CommonFiles/lite/examples', ...
+    'CommonFiles/plottingFunctions', ...
+    'CommonFiles/simulinkBased/Library2014b')
+
+% Make sure we can run getting started.
+run doc/GettingStarted
+
+projectFile = 'HybridEquationsToolbox.prj';
 matlab.addons.toolbox.packageToolbox(projectFile)
