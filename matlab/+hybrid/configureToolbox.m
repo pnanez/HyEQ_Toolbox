014077a943b7a6b1f224f965f30347c54771c657
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2022-08-28 21:10:28 -0700

    Modify hybrid.configureToolbox to upgrade Simulink example models.

--- matlab/+hybrid/configureToolbox.m
+++ matlab/+hybrid/configureToolbox.m
@@ -5,7 +5,7 @@ function configureToolbox()
 % 2. Prompts the user to run automated tests.
 % 3. Enables autocomplete data for the MATLAB editor in supported versions of
 %    MATLAB. 
-% 4. Saves the Simulink Library file with the current MATLAB version.
+% 4. Saves the Simulink library and example model files with the current MATLAB version.
 % 
 % Added in HyEQ Toolbox version 3.0 
 
@@ -18,7 +18,8 @@ if length(hyeqsolver_paths) > 1
         for i = 1:length(hyeqsolver_paths)
             path_to_solver = fileparts(hyeqsolver_paths{i});
 
-            toolbox_root_path_candidate = what(fullfile(path_to_solver, '..', '..')).path;
+            file = what(fullfile(path_to_solver, '..', '..'));
+            toolbox_root_path_candidate = file.path;
             uninstall_script_candidate_path = fullfile(toolbox_root_path_candidate, 'tbclean.m');
 
             if exist(uninstall_script_candidate_path, 'file') == 2
@@ -44,20 +45,55 @@ elseif isempty(hyeqsolver_paths)
     error('The Hybrid Equations Toolbox is not installed.')
 end
 
-% Enable Autocomplete information (if supported)
+%% Open and save the Simulink Library on the current version.
+
+% Temporarily disable saving a backup of the old Simulink models when saving on
+% a new version of Simulink.
+set_param(0,'AutoSaveOptions', struct('SaveBackupOnVersionUpgrade', false))
+cleanup_obj_backup_setting = onCleanup( ...
+    @() set_param(0,'AutoSaveOptions', struct('SaveBackupOnVersionUpgrade', false)));
+
+% Temporarily disable a warning when loading an old Simulink model.
+warning('off', 'Simulink:Commands:LoadingOlderModel')
+cleanup_obj_warning = onCleanup( ...
+    @() warning('off', 'Simulink:Commands:LoadingOlderModel'));
+
+% Close all open systems.
+while ~isempty(gcs)
+    try
+        close_system(gcs())
+    catch e
+        if strcmp(e.identifier, 'Simulink:Commands:InvModelDirty')
+            warning(['The open Simulink model "', gcs(), '" has been modified ' ...
+                'and cannot be closed automatically. Please manually close ' ...
+                'all models and run hybrid.configureToolbox() again.'])
+            rethrow(e)
+        end
+    end
+end
+
+save_simulink_model_on_current_version('HyEQ_Library')
+save_simulink_model_on_current_version('hybrid.examples.analog_to_digital_converter.adc')
+save_simulink_model_on_current_version('hybrid.examples.behavior_in_C_intersection_D.hybrid_priority')
+save_simulink_model_on_current_version('hybrid.examples.bouncing_ball.bouncing_ball')
+save_simulink_model_on_current_version('hybrid.examples.bouncing_ball_with_adc.ball_with_adc')
+save_simulink_model_on_current_version('hybrid.examples.bouncing_ball_with_input.ball_with_input')
+save_simulink_model_on_current_version('hybrid.examples.bouncing_ball_with_input.ball_with_input2')
+save_simulink_model_on_current_version('hybrid.examples.coupled_subsystems.coupled')
+save_simulink_model_on_current_version('hybrid.examples.finite_state_machine.fsm')
+save_simulink_model_on_current_version('hybrid.examples.fireflies.fireflies')
+save_simulink_model_on_current_version('hybrid.examples.mobile_robot.mobile_robot')
+save_simulink_model_on_current_version('hybrid.examples.network_estimation.network')
+save_simulink_model_on_current_version('hybrid.examples.vehicle_on_constrained_path.vehicle_on_path')
+save_simulink_model_on_current_version('hybrid.examples.zero_order_hold.zoh')
+save_simulink_model_on_current_version('hybrid.examples.zoh_feedback_control.zoh_feedback')
+
+%% Enable Autocomplete information (if supported)
 if ~verLessThan('matlab','9.10')
     enable_enhanced_autocomplete(hyeqsolver_paths)
 end
 
-% Open and save the Simulink Library
-close_system('HyEQ_Library', 0) % Ensure there is not an old version of the library open.
-% Open, save, and close the new version.
-file_path = fullfile(hybrid.getFolderLocation('Simulink'), 'HyEQ_Library.slx');
-open_system(file_path)
-save_system(file_path)
-close_system(file_path)
-
-% Prompt to run automated tests.
+%% Prompt to run automated tests.
 promptMessage = sprintf(['Do you want to run automated tests?\n' ...
                     'The tests will take less than a minute.']);
 button = questdlg(promptMessage, 'Run Tests', 'Run Tests', 'Skip Tests', 'Run Tests');
@@ -94,6 +130,22 @@ elseif ~exist(p_after, 'file')
         p_before, p_after)
 end
 if exist(p_after, 'file')
-    disp('Autocomplete information is enabled for the Hybrid Equations Toolbox.')
+    disp('Enabled autocomplete information for the Hybrid Equations Toolbox.')
 end
+end
+
+function save_simulink_model_on_current_version(name)
+     % Ensure there is not a version of the model open.
+    close_system(name, 0)
+
+    % Get the path for the model file.
+    file_path = which(name);
+
+    % Open, save, and close.
+    load_system(file_path) % load_system is much faster than open_system.
+    save_system(file_path)
+    close_system(file_path)
+
+    % Tell user what we did
+    fprintf('Updated Simulink model "%s" to the current version of Simulink.\n', name)
 end
